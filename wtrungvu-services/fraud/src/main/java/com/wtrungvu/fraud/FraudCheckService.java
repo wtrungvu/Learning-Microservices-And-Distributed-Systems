ac8d4fc50cafe93f395d3d2a55a1a1ea55be6609
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-11 17:33:03 +0700

    Fraud Controller Service & Repository ✨

--- wtrungvu-services/fraud/src/main/java/com/wtrungvu/fraud/FraudCheckService.java
+++ wtrungvu-services/fraud/src/main/java/com/wtrungvu/fraud/FraudCheckService.java
@@ -1,17 +1,15 @@
 package com.wtrungvu.fraud;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@AllArgsConstructor
 public class FraudCheckService {
 
     private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
-
-    public FraudCheckService(FraudCheckHistoryRepository fraudCheckHistoryRepository) {
-        this.fraudCheckHistoryRepository = fraudCheckHistoryRepository;
-    }
 
     public boolean isFraudulentCustomer(Integer customerId) {
         fraudCheckHistoryRepository.save(
--- wtrungvu-services/fraud/src/main/java/com/wtrungvu/fraud/FraudController.java
+++ wtrungvu-services/fraud/src/main/java/com/wtrungvu/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.wtrungvu.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
-    private final FraudService fraudService;
+    private final FraudCheckService fraudCheckService;
 
     @GetMapping (path = "{customerId}")
     public FraudCheckResponse isFraudster(
             @PathVariable("customerId") Integer customerId) {
-        boolean isFraudulentCustomer = fraudService
+        boolean isFraudulentCustomer = fraudCheckService
                 .isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }

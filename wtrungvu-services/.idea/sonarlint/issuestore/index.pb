ebb27ffaf5562a0943f578145e60c675622ab43e
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-11 12:34:59 +0700

    Connecting to DB :wrench:

---
+++ wtrungvu-services/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+u
+Ecustomer/src/main/java/com/wtrungvu/customer/CustomerApplication.java,f/2/f2fe65f648c3ba0bd03dfbb763778f20eb49cd6b
+t
+Dcustomer/src/main/java/com/wtrungvu/customer/CustomerController.java,e/d/eddb1162fa72761b71ad202db24c5fa9f9b44c3b
+}
+Mcustomer/src/main/java/com/wtrungvu/customer/CustomerRegistrationRequest.java,6/1/612972e4b25257d44e9c2e99299359ead1d1f088
--- wtrungvu-services/customer/src/main/java/com/wtrungvu/customer/CustomerService.java
+++ wtrungvu-services/customer/src/main/java/com/wtrungvu/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.wtrungvu.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -13,5 +13,6 @@ public record CustomerService() {
         // todo: check if email is valid
         // todo: check if email is not already registered
         // todo: store customer in database
+        customerRepository.save(customer);
     }
 }

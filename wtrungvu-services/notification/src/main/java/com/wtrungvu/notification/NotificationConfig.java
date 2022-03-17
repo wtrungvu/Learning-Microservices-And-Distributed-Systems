9d69aa90ce24f22011dbf8ebe639287e94f83ae9
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-17 15:49:03 +0700

    Queue Topic and Binding :sparkles:

--- wtrungvu-services/notification/src/main/java/com/wtrungvu/notification/NotificationConfig.java
+++ wtrungvu-services/notification/src/main/java/com/wtrungvu/notification/NotificationConfig.java
@@ -1,6 +1,11 @@
 package com.wtrungvu.notification;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,11 +19,29 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-key.internal-notification}")
     private String internalNotificationRoutingKey;
+    
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+    
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 
     public String getInternalExchange() {
         return internalExchange;
     }
-
+    
     public void setInternalExchange(String internalExchange) {
         this.internalExchange = internalExchange;
     }
